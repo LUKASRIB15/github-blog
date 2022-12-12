a54933d432acadfb7a7370984f819c532cde460a
lucas <lucasoribeiro100@gmail.com>
2022-12-12 09:52:36 -0300

    Realizando a configuração basica do styled-components e do css global do projeto

---
+++ src/styles/global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    *{
+        margin:0;
+        padding:0;
+        box-sizing: border-box;
+    }
+
+    body{
+        background-color: ${props=>props.theme["base-background"]};
+        color: ${props=>props.theme["base-title"]};
+    }
+
+    body, input, textarea, button{
+        font-family: "Nunito", sans-serif;
+        font-size: 1rem;
+        font-weight: 400;
+    }
+
+    
+`
