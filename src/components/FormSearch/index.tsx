12c58768c5365979492bb15dc278ff902640198b
Lucas Ribeiro <100320271+LUKASRIB15@users.noreply.github.com>
2022-12-14 15:51:56 -0300

    Validação do FormSearch (#20)
    Co-authored-by: lucas <lucasoribeiro100@gmail.com>

--- src/components/FormSearch/index.tsx
+++ src/components/FormSearch/index.tsx
@@ -1,14 +1,34 @@
 import { InputFormSearch, HeaderFormSearch, LayoutFormSearch } from "./styles";
+import * as z from "zod"
+import {zodResolver} from "@hookform/resolvers/zod"
+import {useForm} from "react-hook-form"
 
 export function FormSearch(){
+    const FormSearchValidationSchema = z.object({
+        query: z.string(),
+    })
+
+    type FormSearchValidationSchemaProps = z.infer<typeof FormSearchValidationSchema>
+
+    const {handleSubmit, register} = useForm<FormSearchValidationSchemaProps>({
+        resolver: zodResolver(FormSearchValidationSchema),
+    })
+
+    function handleSearchForm(data:FormSearchValidationSchemaProps){
+        console.log(data)
+    }
     return(
         <LayoutFormSearch>
             <HeaderFormSearch>
                 <strong>Publicações</strong>
                 <span>13 publicações</span>
             </HeaderFormSearch>
-            <form>
-                <InputFormSearch type="text" placeholder="Buscar conteúdo"/>
+            <form onSubmit={handleSubmit(handleSearchForm)}>
+                <InputFormSearch 
+                    type="text" 
+                    placeholder="Buscar conteúdo"
+                    {...register("query")} 
+                />
             </form>
         </LayoutFormSearch>
     )
