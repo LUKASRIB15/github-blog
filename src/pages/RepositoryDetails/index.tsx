2ba667f3313cb80c0ed661ccf0796b30bafc1bdc
lucas <lucasoribeiro100@gmail.com>
2022-12-14 15:03:21 -0300

    Termino da página RepositoryDetails

--- src/pages/RepositoryDetails/index.tsx
+++ src/pages/RepositoryDetails/index.tsx
@@ -1,10 +1,24 @@
 import { CardRepository } from "../../components/CardRepository";
-import { LayoutMainRepositoryDetails } from "./styles";
+import { DescriptionRepositoryDetails, LayoutMainRepositoryDetails } from "./styles";
 
 export function RepositoryDetails(){
     return(
         <LayoutMainRepositoryDetails>
             <CardRepository/>
+            <DescriptionRepositoryDetails>
+                <p>
+                    <strong>Programming languages all have built-in data structures, but these often differ from one language to another.</strong> 
+                    This article attempts to list the built-in data structures available in JavaScript and what properties they have. 
+                    These can be used to build other data structures.
+                    Wherever possible, comparisons with other languages are drawn.
+                </p>
+                <p>
+                    <a href="#">Dynamic typing</a>
+                </p>
+                <p>
+                    JavaScript is a loosely typed and dynamic language. Variables in JavaScript are not directly associated with any particular value type, and any variable can be assigned (and re-assigned) values of all types:
+                </p>
+            </DescriptionRepositoryDetails>
         </LayoutMainRepositoryDetails>
     )
 }
--- src/pages/RepositoryDetails/styles.ts
+++ src/pages/RepositoryDetails/styles.ts
@@ -3,3 +3,13 @@ import styled from "styled-components"
 export const LayoutMainRepositoryDetails = styled.main`
     margin: -5.5rem 18rem 0rem;
 `
+
+export const DescriptionRepositoryDetails = styled.div`
+    padding: 2.5rem 2rem;
+    color: ${props=>props.theme["base-text"]};
+    line-height: 1.6;
+    
+    a{
+        color:${props=>props.theme["blue"]};
+    }
+`
