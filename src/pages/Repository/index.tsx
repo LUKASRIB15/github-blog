00fee48b10fd9ff1131190c91e4f2df9ce62b880
LUKASRIB15 <lucas.ribeiro06@aluno.ifce.edu.br>
2023-07-28 07:57:09 -0300

    Mostrando em tela os dados da página de repositório e usando react-markdown para mostrar o Readme

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,38 +1,73 @@
 import { ProfileRepository } from '../../components/ProfileRepository'
 import { RepositoryLayout } from './styles'
-import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import { api } from '../../lib/api'
+import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import ReactMarkDown from 'react-markdown'
 import { Light as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import remarkGfm from 'remark-gfm'
 
 export function Repository() {
+  const { id } = useParams()
+  const [readme, setReadme] = useState<string | null>(null)
+
+  async function getReadmeContent() {
+    const response = await api.get(`/repos/LUKASRIB15/${id}/readme`, {
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+      },
+    })
+
+    if (response.status === 200) {
+      const data = response.data
+      if (data.content) {
+        const readmeContent = atob(data.content)
+        setReadme(readmeContent)
+      }
+    }
+  }
+
+  useEffect(() => {
+    getReadmeContent()
+  }, [])
+
   return (
     <RepositoryLayout>
       <ProfileRepository />
       <main>
-        <p>
-          Programming languages all have built-in data structures, but these
-          often differ from one language to another. This article attempts to
-          list the built-in data structures available in JavaScript and what
-          properties they have. These can be used to build other data
-          structures. Wherever possible, comparisons with other languages are
-          drawn. Dynamic typing JavaScript is a loosely typed and dynamic
-          language. Variables in JavaScript are not directly associated with any
-          particular value type, and any variable can be assigned (and
-          re-assigned) values of all types:
-        </p>
-        <SyntaxHighlighter
-          language="typescript"
-          style={docco}
-          customStyle={{
-            padding: '1rem',
-            background: '#112131',
-            color: '#FFFFFF',
-            margin: '1.5rem 0',
-          }}
-        >
-          {`let foo = 42; // foo is now a number 
-foo = ‘bar’; // foo is now a string 
-foo = true; // foo is now a boolean`}
-        </SyntaxHighlighter>
+        {readme ? (
+          <ReactMarkDown
+            children={readme}
+            components={{
+              code({ node, inline, className, children, ...props }) {
+                const match = /language-(\w+)/.exec(className || '')
+                return !inline && match ? (
+                  <SyntaxHighlighter
+                    {...props}
+                    children={String(children).replace(/\n$/, '')}
+                    style={dracula}
+                    language={match[1]}
+                    PreTag="div"
+                    customStyle={{
+                      padding: '1rem',
+                      background: '#112131',
+                      color: '#FFFFFF',
+                      margin: '1.5rem 0',
+                    }}
+                  />
+                ) : (
+                  <code {...props} className={className}>
+                    {children}
+                  </code>
+                )
+              },
+            }}
+            remarkPlugins={[remarkGfm]}
+          />
+        ) : (
+          <p>Carregando o conteúdo do README...</p>
+        )}
       </main>
     </RepositoryLayout>
   )
